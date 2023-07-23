c3a1ed21c4bda8cd5c23344c2ce3decb7862888b
IMENDOZA <imendoza@unab.edu.co>
2023-07-23 14:37:53 -0500

    ADD - Servicio para obtener registros por un rango de fechas

--- src/main/java/com/pruebaTecnica/ticketsApp/repositories/TicketRepository.java
+++ src/main/java/com/pruebaTecnica/ticketsApp/repositories/TicketRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.OffsetDateTime;
 import java.util.List;
 
 @Repository
@@ -17,5 +18,7 @@ public interface TicketRepository extends JpaRepository<Ticket, Long> {
     List<Ticket> findAllByUser(String user);
 
     Page<Ticket> findAllByUser(String user, Pageable pageable);
+
+    Page<Ticket> findByCreatedAtBetween(OffsetDateTime fechaInicio, OffsetDateTime fechafin, Pageable pageable);
 
 }
--- src/main/java/com/pruebaTecnica/ticketsApp/services/TicketGraphQLService.java
+++ src/main/java/com/pruebaTecnica/ticketsApp/services/TicketGraphQLService.java
@@ -11,7 +11,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
 
+import java.time.LocalDate;
 import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 
 @Component
@@ -52,6 +55,24 @@ public class TicketGraphQLService implements GraphQLQueryResolver, GraphQLMutati
     public Page<Ticket> findAllByUserPaged(String user, Integer page, Integer size){
         Pageable pageable = PageRequest.of(page, size);
         return repository.findAllByUser(user, pageable);
+    }
+
+    public Page<Ticket> findByRangoFechas(String fechaInicio, String fechaFin, Integer page, Integer size) throws Exception {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        try {
+            // Realizar el parse del texto a LocalDate
+            LocalDate localDate = LocalDate.parse(fechaInicio, formatter);
+            LocalDate localDate2 = LocalDate.parse(fechaFin, formatter);
+            // Convertir LocalDate a OffsetDateTime usando el offset de zona horaria deseado
+            ZoneOffset zoneOffset = ZoneOffset.of("-05:00"); // Offset de zona horaria deseado
+            OffsetDateTime offsetDateTime = localDate.atStartOfDay().atOffset(zoneOffset);
+            OffsetDateTime offsetDateTime2 = localDate2.atTime(23,59,59).atOffset(zoneOffset);
+            Pageable pageable = PageRequest.of(page, size);
+            return repository.findByCreatedAtBetween(offsetDateTime, offsetDateTime2, pageable);
+        } catch (Exception e) {
+            System.out.println("Error al hacer el parse del texto: " + e.getMessage());
+            throw new Exception("El formato de fecha es incorrecta, el formato adecuado es yyyy-MM-dd");
+        }
     }
 
     public Ticket createTicket(String user, Boolean status) {
