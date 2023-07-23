f3fcf72c0f17a50cf681b1dfc443df8f4357e5b8
IMENDOZA <imendoza@unab.edu.co>
2023-07-23 09:32:32 -0500

    ADD - Modelo ticket

---
+++ src/main/java/com/pruebaTecnica/ticketsApp/models/ticket.java
@@ -0,0 +1,33 @@
+package com.pruebaTecnica.ticketsApp.models;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Comment;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity @Getter @Setter
+@Table(name = "tickets", catalog = "ticketsDB")
+public class ticket {
+    @Id
+    @Column(name = "Id", insertable = false, unique = true)
+    @Comment("Identificador de la tabla")
+    private Long id;
+    @Basic
+    @Column(name = "UserName", length = 100, nullable = false)
+    @Comment("Nombre de usuario")
+    private String user;
+    @Basic
+    @Column(name = "CreatedAt", nullable = false)
+    @Comment("Fecha de creacion del ticket")
+    private Timestamp createdAt;
+    @Basic
+    @Column(name = "UpdatedAt")
+    @Comment("Fecha de actualizacion del ticket")
+    private Timestamp updatedAt;
+    @Basic
+    @Column(name = "Status", nullable = false)
+    @Comment("Estado en el que se encuentra un ticket; abierto(1) cerrado(0)")
+    private Boolean status;
+}
