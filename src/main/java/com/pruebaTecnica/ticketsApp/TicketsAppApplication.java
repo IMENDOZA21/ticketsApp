95e3e0d79836662a8cfef198387950aa531691d5
IMENDOZA <imendoza@unab.edu.co>
2023-07-23 08:05:51 -0500

    Se inicia proyecto ticketsApp

---
+++ src/main/java/com/pruebaTecnica/ticketsApp/TicketsAppApplication.java
@@ -0,0 +1,13 @@
+package com.pruebaTecnica.ticketsApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pruebaTecnica/ticketsApp/TicketsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruebaTecnica.ticketsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
